9a5bbf0977c8a521573049676c86ab031cec8f7f
404ss <raynii0x@gmail.com>
2026-03-02 17:09:59 +0800

    3.2

---
+++ src/ArrayListTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("11");
+        list.add("22");
+        list.add("33");
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.size());
+
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ void main() {
     IO.println(String.format(" swelcome!"));
 
     for (int i = 1; i <= 5; i++) {
-        //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-        // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-        IO.println("i = " + i);
+        System.out.println("i = " + i);
     }
 
 }
